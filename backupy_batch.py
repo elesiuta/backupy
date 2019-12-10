dfa44d34d5b87613ad2f444ded86fc6af4625392
eric <elesiuta@gmail.com>
2019-12-10 10:12:29 -0500

    Check that the directory exists first so it can continue with the next instead of exiting

--- backupy_batch.py
+++ backupy_batch.py
@@ -2,6 +2,7 @@ import os
 import backupy
 source_list = []
 for source in source_list:
-    backup_manager = backupy.BackupManager({"source": source, "load": True})
-    backup_manager.backup()
+    if os.path.isdir(source):
+        backup_manager = backupy.BackupManager({"source": source, "load": True})
+        backup_manager.backup()
 os.system("pause")
