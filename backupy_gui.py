1bd0ffd0575db904aaa76a327105acea1dcee32c
eric <elesiuta@gmail.com>
2019-12-17 13:02:36 -0500

    add license notice

--- backupy_gui.py
+++ backupy_gui.py
@@ -11,6 +11,19 @@ from gooey import Gooey, GooeyParser
 import backupy
 sys.stdout = sys.__stdout__
 
+GPLv3 = """This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+
 def colourize(string: str, colour: str) -> str:
     colours = {
             "HEADER" : fg("magenta"),
@@ -47,7 +60,7 @@ def simplePrompt(msg: str) -> str:
                     'description': 'A simple python program for backing up directories',
                     'version': '0.5.5',
                     'website': 'https://github.com/elesiuta/backupy',
-                    'license': 'GPLv3'
+                    'license': GPLv3
                 }]
             }])
 def main_gui():
