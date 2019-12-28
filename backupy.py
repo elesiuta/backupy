e2c14199a3142bad33f4a842596d833f52200817
eric <elesiuta@gmail.com>
2019-12-27 20:45:31 -0500

    add github link to source

--- backupy.py
+++ backupy.py
@@ -11,6 +11,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# https://github.com/elesiuta/backupy
+
 import os
 import argparse
 import csv
