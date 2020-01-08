a40d6a6812f535f9c8d8d9604b35a8ed8c2bd9e7
eric <elesiuta@gmail.com>
2020-01-07 21:19:05 -0500

    update usage message based on docopt style

--- backupy.py
+++ backupy.py
@@ -888,7 +888,11 @@ class BackupManager:
 def main():
     parser = argparse.ArgumentParser(description=getString("BackuPy: A simple backup program in python with an emphasis on transparent behaviour"),
                                      formatter_class=lambda prog: ArgparseCustomFormatter(prog, max_help_position=15),
-                                     usage="%(prog)s [--help] source [dest] [options]")
+                                     usage="%(prog)s <source> <dest> [options]\n"
+                                           "       %(prog)s <source> --load [--norun]\n"
+                                           "       %(prog)s -h | --help\n"
+                                           "note: options can also precede source,\n"
+                                           "      however -f and -ff are greedy and cannot directly precede")
     parser.add_argument("source", action="store", type=str,
                         help=getString("Path of source"))
     parser.add_argument("dest", action="store", type=str, nargs="?", default=None,
