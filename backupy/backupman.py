0187dd602f9d06175b920918dea71deb46288120
eric <elesiuta@gmail.com>
2020-06-19 00:06:44 -0400

    update consistency check since selfCompare will now detect crc errors for files that exist on both sides as 'changed' when comparing sides some of the comparisions are extra redundant but left in for readability of what should be checked for hint: use python -m unittest tests.tests.TestBackupy.TESTNAME to run a single test, and move _checkConsistency out of the try except to see which assert failed

--- backupy/backupman.py
+++ backupy/backupman.py
@@ -149,10 +149,11 @@ class BackupManager():
             assert set(redundant_dict_dest["new"]) == dest_new
             redundant_dict_compare = self.source.selfCompare(self.dest.dict_current, False, True, False)
             redundant_dict_compare_reverse = self.dest.selfCompare(self.source.dict_current, False, True, False)
-            assert set(redundant_dict_compare["modified"]) == changed
+            assert set(redundant_dict_compare["modified"]) == (changed | (source_crc_errors & source_dict & dest_dict) | (dest_crc_errors & dest_dict & source_dict))
+            assert set(redundant_dict_compare["modified"]) == (changed | (source_crc_errors - (source_dict - dest_dict)) | (dest_crc_errors - (dest_dict - source_dict)))
             assert set(redundant_dict_compare["new"]) == source_only | source_moved | dest_deleted
             assert set(redundant_dict_compare["missing"]) == dest_only | dest_moved | source_deleted
-            assert set(redundant_dict_compare_reverse["modified"]) == changed
+            assert set(redundant_dict_compare_reverse["modified"]) == (changed | (source_crc_errors - (source_dict - dest_dict)) | (dest_crc_errors - (dest_dict - source_dict)))
             assert set(redundant_dict_compare_reverse["new"]) == dest_only | dest_moved | source_deleted
             assert set(redundant_dict_compare_reverse["missing"]) == source_only | source_moved | dest_deleted
             # might be able to make a check for moved along the lines of source_only is approx source_new | dest_missing - dest_new - source_missing
